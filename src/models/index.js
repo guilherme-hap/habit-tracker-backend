f1e80fb1adc174989a669723c2ea602c0d629dc7
KaueNoites <kauenoites@outlook.com>
2025-06-14 15:00:01 -0300

    back com habitos

--- src/models/index.js
+++ src/models/index.js
@@ -1,14 +1,22 @@
 import Sequelize from 'sequelize';
 import createUserModel from './User.js';
+import createHabitModel from './Habit.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: 'database.sqlite'
 });
 
-const User = createUserModel(sequelize);
+const User = createUserModel(sequelize, Sequelize.DataTypes);
+const Habit = createHabitModel(sequelize, Sequelize.DataTypes);
+
+User.hasMany(Habit, { foreignKey: 'userId' });
+Habit.belongsTo(User, { foreignKey: 'userId' });
+
 sequelize.authenticate()
   .then(() => console.log('Conexão com o banco de dados estabelecida com sucesso.'))
   .catch(err => console.error('Não foi possível conectar ao banco de dados:', err));
 
-export { sequelize, User };
+export { sequelize, User, Habit };
